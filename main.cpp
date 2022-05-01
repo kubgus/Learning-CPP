b20b72f8baa7fbbe771a648d54a641cc27a3241e
kubgus <53797257+kubgus@users.noreply.github.com>
2022-05-02 00:06:23 +0200

    Inital Commit - Hello World

--- main.cpp
+++ main.cpp
@@ -4,14 +4,7 @@ using namespace std;
 
 int main()
 {
-    string noun = "apples";
-    string verb = "hurts";
-    int age = 69;
-
-    cout << "I really like " << noun << "." << endl;
-    cout << "My hand " << verb << " very much." << endl;
-    cout << "I am " << age << " years old." << endl;
-    cout << "The first word was " << noun << "." << endl;
+    cout << "Hello World!" << endl;
 
     return 0;
 }
