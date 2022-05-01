193bd9aa6cc62be090e04f4974d995fc7b511d6c
kubgus <53797257+kubgus@users.noreply.github.com>
2022-05-02 00:01:40 +0200

    Variables

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string noun = "apples";
+    string verb = "hurts";
+    int age = 69;
+
+    cout << "I really like " << noun << "." << endl;
+    cout << "My hand " << verb << " very much." << endl;
+    cout << "I am " << age << " years old." << endl;
+    cout << "The first word was " << noun << "." << endl;
+
+    return 0;
+}
