d16d5479ddbe579ac9cd6724b02c17dc095e55ab
kubgus <53797257+kubgus@users.noreply.github.com>
2022-05-04 20:13:10 +0200

    Functions

--- main.cpp
+++ main.cpp
@@ -2,14 +2,30 @@
 
 using namespace std;
 
+void sayGoodbye(string name)
+{
+    cout << "Goodbye, " << name << "..." << endl;
+}
+
+void sayHi(string name, int age);
+//^ This line defines the function at the top
+//^ so the actual code can be at the bottom
+//^ The code in the curly brackets can go up here
+//^ as well, this is just more pleasant to read like this
+
 int main()
 {
-    char grade = 'A';
-    string phrase = "Pretty cool stuff...";
-    int age = 14;
-    float decimal = 3.14;
-    double pi = 3.14159;
-    bool knowledge = true;
+    sayHi("Jakub", 14);
+    sayHi("Mike", 120);
+    sayHi("Steve", 64);
+
+    sayGoodbye("Rick Astley");
 
     system("pause");
+}
+
+void sayHi(string name, int age)
+{
+    cout << "Hello, " << name << "!!" << endl;
+    cout << "You are " << age << " years old." << endl;
 }
