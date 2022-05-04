7da6d8235d368623f00beacf52aec259b4698bb9
kubgus <53797257+kubgus@users.noreply.github.com>
2022-05-04 17:20:38 +0200

    Output Directly in Source Code

--- main.cpp
+++ main.cpp
@@ -5,15 +5,15 @@ using namespace std;
 int main()
 {
     string phrase = "My name is kubgus!";
-    cout << phrase << endl;
-    cout << phrase.length() << endl;
-    cout << phrase[0] << endl;
+    cout << phrase << endl;          // My name is kubgus!
+    cout << phrase.length() << endl; // 18
+    cout << phrase[0] << endl;       // M
     phrase[0] = 'B';
-    cout << phrase << endl;
-    cout << phrase.find("By", 0) << endl;
-    cout << phrase.find("kubgus", 5) << endl;
+    cout << phrase << endl;                   // By name is kubgus!
+    cout << phrase.find("By", 0) << endl;     // 0
+    cout << phrase.find("kubgus", 5) << endl; // 11
     string phrasePart = phrase.substr(3, 4);
-    cout << phrasePart << endl;
+    cout << phrasePart << endl; // name
 
     system("pause");
 }
