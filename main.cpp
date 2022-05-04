459012a0815106666212ae94ca870f8c8f4c6a63
kubgus <53797257+kubgus@users.noreply.github.com>
2022-05-04 16:07:59 +0200

    Data Types

--- main.cpp
+++ main.cpp
@@ -4,14 +4,13 @@ using namespace std;
 
 int main()
 {
-    string noun = "apples";
-    string verb = "hurts";
-    int age = 69;
 
-    cout << "I really like " << noun << "." << endl;
-    cout << "My hand " << verb << " very much." << endl;
-    cout << "I am " << age << " years old." << endl;
-    cout << "The first word was " << noun << "." << endl;
+    char grade = 'A';
+    string phrase = "Pretty cool stuff...";
+    int age = 14;
+    float decimal = 3.14;
+    double pi = 3.14159;
+    bool knowledge = true;
 
     return 0;
 }
