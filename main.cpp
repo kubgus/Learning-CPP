22a68c4f82e627821bd80f466235cc03d0cb4db4
kubgus <53797257+kubgus@users.noreply.github.com>
2022-05-04 17:34:46 +0200

    Input

--- main.cpp
+++ main.cpp
@@ -1,19 +1,23 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-    string phrase = "My name is kubgus!";
-    cout << phrase << endl;          // My name is kubgus!
-    cout << phrase.length() << endl; // 18
-    cout << phrase[0] << endl;       // M
-    phrase[0] = 'B';
-    cout << phrase << endl;                   // By name is kubgus!
-    cout << phrase.find("By", 0) << endl;     // 0
-    cout << phrase.find("kubgus", 5) << endl; // 11
-    string phrasePart = phrase.substr(3, 4);
-    cout << phrasePart << endl; // name
+    int age;
+    cout << "Enter your age: "; // no endl
+    cin >> age;
+    cout << "You are " << age << " years old." << endl;
+
+    string name;
+    cout << "Enter your name: "; // no endl
+    cin.clear();
+    cin.sync();
+    //^ These two lines flush the input buffer after one
+    //^ input has already been registered
+    getline(cin, name);
+    cout << "Hello, " << name << "!" << endl;
 
     system("pause");
 }
