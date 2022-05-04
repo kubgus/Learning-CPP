1109c9c4a26ec7bb393890313dec7f6a9d99dd11
kubgus <53797257+kubgus@users.noreply.github.com>
2022-05-04 20:38:55 +0200

    If Statements

--- main.cpp
+++ main.cpp
@@ -4,12 +4,36 @@ using namespace std;
 
 int main()
 {
-    char grade = 'A';
-    string phrase = "Pretty cool stuff...";
-    int age = 14;
-    float decimal = 3.14;
-    double pi = 3.14159;
     bool knowledge = true;
+    bool confidence = true;
+
+    // I didn't choose to write curly brackets on the next line, my formatter did, sorry
+
+    if (knowledge && confidence)
+    {
+        cout << "You are smart and confident." << endl;
+    }
+    else if (knowledge && !confidence)
+    {
+        cout << "You are smart, but you don't like talking about it." << endl;
+    }
+    else if (!knowledge && confidence)
+    {
+        cout << "You pretend to be smart." << endl;
+    }
+    else
+    {
+        cout << "You just need more time." << endl;
+    }
+
+    if (knowledge || confidence)
+    {
+        cout << "At least one parameter is true." << endl;
+    }
+    else
+    {
+        cout << "Both parameters are false." << endl;
+    }
 
     system("pause");
 }
