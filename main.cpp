0276b6ee261e86c8e5eb996e48afaf5cc473bbb5
kubgus <53797257+kubgus@users.noreply.github.com>
2022-05-04 19:47:14 +0200

    Arrays

--- main.cpp
+++ main.cpp
@@ -4,13 +4,16 @@ using namespace std;
 
 int main()
 {
-
-    char grade = 'A';
-    string phrase = "Pretty cool stuff...";
-    int age = 14;
-    float decimal = 3.14;
-    double pi = 3.14159;
-    bool knowledge = true;
+    int favoriteNumbers[] = {7, 11, 69, 16, 32, 64};
+    cout << favoriteNumbers[0] << endl; // 7
+    cout << favoriteNumbers[2] << endl; // 69
+    favoriteNumbers[0] = 13;
+    cout << favoriteNumbers[0] << endl; // 13
+    string coolWords[10];
+    //               ^ set array length
+    coolWords[5] = "incomprehensibilities";
+    cout << coolWords[5] << endl; // incomprehensibilities
+    cout << coolWords[4] << endl; // *null*
 
     system("pause");
 }
