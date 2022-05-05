71fc8672885a61f14682fc0884dd7eb43b659fbc
kubgus <53797257+kubgus@users.noreply.github.com>
2022-05-05 22:33:33 +0200

    Nested For Loops

--- main.cpp
+++ main.cpp
@@ -4,14 +4,18 @@ using namespace std;
 
 int main()
 {
-    // 2D Array / Matrix
     int grid[3][2] = {{1, 2},
                       {3, 4},
                       {5, 6}};
 
-    cout << grid[0][0] << endl; // 1
-    cout << grid[1][0] << endl; // 3
-    cout << grid[2][1] << endl; // 6
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            cout << grid[i][j]; // 1-6
+        }
+        cout << endl;
+    }
 
     system("pause");
 }
