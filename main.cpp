bcfc96e8474f51d4d5150039675b5c297b11e4ab
kubgus <53797257+kubgus@users.noreply.github.com>
2022-05-05 18:00:18 +0200

    While Loops

--- main.cpp
+++ main.cpp
@@ -4,12 +4,21 @@ using namespace std;
 
 int main()
 {
-    char grade = 'A';
-    string phrase = "Pretty cool stuff...";
-    int age = 14;
-    float decimal = 3.14;
-    double pi = 3.14159;
-    bool knowledge = true;
+    // Always checks the condition before running the code (while loop)
+    int index1 = 1;
+    while (index1 <= 5)
+    {
+        cout << index1 << endl; // 1-5
+        index1++;
+    }
+
+    // Executes the code first before checking the condition to run again (do-while loop)
+    int index2 = 6;
+    do
+    {
+        cout << index1 << endl; // 6
+        index1++;
+    } while (index2 <= 5);
 
     system("pause");
 }
