92a3e7748520f31a145f490675d75a0c95e5f5fe
kubgus <53797257+kubgus@users.noreply.github.com>
2022-05-06 16:43:57 +0200

    Classes and Objects

--- main.cpp
+++ main.cpp
@@ -2,14 +2,33 @@
 
 using namespace std;
 
+class Book
+{
+public:
+    string title;
+    string author;
+    int pages;
+};
+
 int main()
 {
-    char grade = 'A';
-    string phrase = "Pretty cool stuff...";
-    int age = 14;
-    float decimal = 3.14;
-    double pi = 3.14159;
-    bool knowledge = true;
+    Book book1;
+    book1.title = "Harry Potter";
+    book1.author = "JK Rowling";
+    book1.pages = 300;
+
+    cout << book1.title << endl;
+    cout << book1.author << endl;
+    cout << book1.pages << endl;
+
+    Book book2;
+    book2.title = "Ur Special";
+    book2.author = "Danny Casale";
+    book2.pages = 70;
+
+    cout << book2.title << endl;
+    cout << book2.author << endl;
+    cout << book2.pages << endl;
 
     system("pause");
 }
