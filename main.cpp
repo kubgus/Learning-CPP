3c2fefb09323c16c3b9183c949364a208dfe7bca
kubgus <53797257+kubgus@users.noreply.github.com>
2022-05-06 21:03:29 +0200

    Get and Set

--- main.cpp
+++ main.cpp
@@ -2,33 +2,53 @@
 
 using namespace std;
 
-class Book
+class Movie
 {
+private:
+    string rating;
+
 public:
     string title;
-    string author;
-    int pages;
+    string director;
+
+    Movie(string aTitle, string aDirector, string aRating)
+    {
+        title = aTitle;
+        director = aDirector;
+        setRating(aRating);
+    }
+
+    void setRating(string aRating)
+    {
+        if (aRating == "G" || aRating == "PG" || aRating == "PG-13" || aRating == "R" || aRating == "NR")
+        {
+            rating = aRating;
+        }
+        else
+        {
+            rating = "NR";
+        }
+    }
+
+    string getRating()
+    {
+        return rating;
+    }
 };
 
 int main()
 {
-    Book book1;
-    book1.title = "Harry Potter";
-    book1.author = "JK Rowling";
-    book1.pages = 300;
+    Movie avengers("The Avengers", "Joss Whedon", "PG-13");
 
-    cout << book1.title << endl;  // Harry Potter
-    cout << book1.author << endl; // JK Rowling
-    cout << book1.pages << endl;  // 300
+    cout << avengers.getRating() << endl; // PG-13
 
-    Book book2;
-    book2.title = "Ur Special";
-    book2.author = "Danny Casale";
-    book2.pages = 70;
+    avengers.setRating("Dog");
 
-    cout << book2.title << endl;  // Ur Special
-    cout << book2.author << endl; // Danny Casale
-    cout << book2.pages << endl;  // 70
+    cout << avengers.getRating() << endl; // NR
+
+    avengers.setRating("R");
+
+    cout << avengers.getRating() << endl; // R
 
     system("pause");
 }
