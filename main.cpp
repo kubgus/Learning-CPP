95e07c53733412aff6fb0a237a4f4df09cb4672b
kubgus <53797257+kubgus@users.noreply.github.com>
2022-05-06 16:55:14 +0200

    Constructors

--- main.cpp
+++ main.cpp
@@ -8,27 +8,37 @@ public:
     string title;
     string author;
     int pages;
+
+    Book() // Executed when values are not provided
+    {
+        title = "No Title";
+        author = "No Author";
+        pages = 0;
+    }
+
+    Book(string aTitle, string aAuthor, int aPages)
+    {
+        title = aTitle;
+        author = aAuthor;
+        pages = aPages;
+    }
 };
 
 int main()
 {
-    Book book1;
-    book1.title = "Harry Potter";
-    book1.author = "JK Rowling";
-    book1.pages = 300;
+    Book book1("Harry Potter", "JK Rowling", 300);
+    Book book2("Ur Special", "Ur Special", 70);
 
-    cout << book1.title << endl;
-    cout << book1.author << endl;
-    cout << book1.pages << endl;
+    cout << book1.title << endl; // Harry Potter
+    cout << book2.pages << endl; // 70
 
-    Book book2;
-    book2.title = "Ur Special";
-    book2.author = "Danny Casale";
-    book2.pages = 70;
+    book2.pages = 90;
 
-    cout << book2.title << endl;
-    cout << book2.author << endl;
-    cout << book2.pages << endl;
+    cout << book2.pages << endl; // 90
+
+    Book book3;
+
+    cout << book3.author << endl; // No Author
 
     system("pause");
 }
