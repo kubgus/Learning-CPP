b68265be88317a09e02f993618a7edeb4ae17823
kubgus <53797257+kubgus@users.noreply.github.com>
2022-05-06 16:56:43 +0200

    Added Comments

--- main.cpp
+++ main.cpp
@@ -17,18 +17,18 @@ int main()
     book1.author = "JK Rowling";
     book1.pages = 300;
 
-    cout << book1.title << endl;
-    cout << book1.author << endl;
-    cout << book1.pages << endl;
+    cout << book1.title << endl;  // Harry Potter
+    cout << book1.author << endl; // JK Rowling
+    cout << book1.pages << endl;  // 300
 
     Book book2;
     book2.title = "Ur Special";
     book2.author = "Danny Casale";
     book2.pages = 70;
 
-    cout << book2.title << endl;
-    cout << book2.author << endl;
-    cout << book2.pages << endl;
+    cout << book2.title << endl;  // Ur Special
+    cout << book2.author << endl; // Danny Casale
+    cout << book2.pages << endl;  // 70
 
     system("pause");
 }
